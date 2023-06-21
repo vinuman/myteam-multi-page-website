158a54dd59ea8ad1fcb5a83124d71415015dd7b8
Vinu Cyril <vinucyril@gmail.com>
2023-06-22 01:47:20 +0530

    home-section 3 desktop

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import bgpattern1 from "./assets/bg-pattern-home-1.svg";
 import bgpattern2 from "./assets/bg-pattern-home-2.svg";
 import bgpatter3 from "./assets/bg-pattern-home-3.svg";
+import bgpattern4 from "./assets/bg-pattern-home-4-about-3.svg";
+import bgpattern5 from "./assets/bg-pattern-home-5.svg";
 import personIcon from "./assets/icon-person.svg";
 import cogIcon from "./assets/icon-cog.svg";
 import chartIcon from "./assets/icon-chart.svg";
 import hamburger from "./assets/icon-hamburger.svg";
+import kady from "./assets/avatar-kady.jpg";
+import ayisha from "./assets/avatar-aiysha.jpg";
+import arthur from "./assets/avatar-arthur.jpg";
+import quotes from "./assets/icon-quotes.svg";
 
 const Home = () => {
   return (
@@ -65,6 +71,60 @@ const Home = () => {
         </div>
         <img src={bgpatter3} className="bg-ptn-3"></img>
         <img src={bgpatter3} className="bg-ptn-4"></img>
+      </section>
+      <section className="home-section-3">
+        <img
+          src={bgpattern4}
+          alt="background pattern"
+          className="bg-ptn-5"
+        ></img>
+        <img
+          src={bgpattern5}
+          alt="background pattern"
+          className="bg-ptn-6"
+        ></img>
+        <h2>
+          Delivering real results for top companies. Some of our{" "}
+          <span style={{ color: "#79C8C7" }}>success stories.</span>
+        </h2>
+        <div className="stories-container">
+          <div className="stories">
+            <img className="quotes" src={quotes} alt="icon-quotes"></img>
+            <p className="body2">
+              {" "}
+              “The team perfectly fit the specialized skill set required. They
+              focused on the most essential features helping us launch the
+              platform eight months faster than planned.”
+            </p>
+            <h3>Kady Baker</h3>
+            <p className="body2">Product Manager at Bookmark</p>
+            <img src={kady} alt="profile kady"></img>
+          </div>
+          <div className="stories">
+            <img className="quotes" src={quotes} alt="icon-quotes"></img>
+            <p className="body2">
+              {" "}
+              “We needed to automate our entire onboarding process. The team
+              came in and built out the whole journey. Since going live, user
+              retention has gone through the roof!”
+            </p>
+            <h3>Aiysha Reese</h3>
+            <p className="body2">Founder of Manage</p>
+            <img src={ayisha} alt="profile ayisha"></img>
+          </div>
+          <div className="stories">
+            <img className="quotes" src={quotes} alt="icon-quotes"></img>
+            <p className="body2">
+              {" "}
+              “Amazing. Our team helped us build an app that delivered a new
+              experience for hiring a physio. The launch was an instant success
+              with 100k downloads in the first month.”
+            </p>
+            <h3>Arthur Clarke</h3>
+            <p className="body2">Co-founder of MyPhysio</p>
+            <img src={arthur} alt="profile arthur"></img>
+          </div>
+        </div>
       </section>
     </>
   );
